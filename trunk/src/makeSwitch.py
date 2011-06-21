f458494532a09de8045ea26faef277d1a5dd44a7
nivwiz@gmail.com <nivwiz@gmail.com@f22951cd-3f0e-1722-56e8-8c26f732a3f7>
2011-06-21 19:48:22 +0000

    fixed button appear after install

--- trunk/src/makeSwitch.py
+++ trunk/src/makeSwitch.py
@@ -37,22 +37,36 @@ if __name__ == "__main__":
     p["/etc/init.d"] = ["switch_backend",]
     p["/usr/lib/hildon-desktop"] = ["switchProfByMeeting.py",]
     p["/usr/lib/switchProfByMeeting"] = ["debug.py", "switch_backend.py", "switch_frontend.py",]
-    p["/usr/share/applications/hildon-status-menu"] = ["switchProfByMeeting.desktop",]
     p["/usr/share/icons/hicolor/32x32/hildon"] = ["switchProfile.png",]
     p["/etc/sudoers.d"] = ["switchProfByMeeting.sudoers",]
-
+    p["/usr/share/applications/hildon-status-menu"] = ["switchProfByMeeting.desktop",]
+    
     p.postinstall = """#!/bin/sh
-update-sudoers
-#no need according to MohammadAG pkill status
+update-sudoers || true
+
+gtk-update-icon-cache /usr/share/icons/hicolor/
+
 #chmod 755".join(p['/usr/...']
 for file in /etc/init.d/switch_backend /usr/lib/switchProfByMeeting/switch_frontend.py /usr/lib/switchProfByMeeting/switch_backend.py /usr/lib/hildon-desktop/switchProfByMeeting.py ; do
 chmod 755 $file
 done
+
+#Force applet reloading to get the icon
+  echo "Reloading openvpn-applet"
+  TMPFILE=`mktemp /tmp/temp.XXXXXX`
+  mv /usr/share/applications/hildon-status-menu/switchProfByMeeting.desktop $TMPFILE
+  sleep 2
+  mv $TMPFILE /usr/share/applications/hildon-status-menu/switchProfByMeeting.desktop
+  rm -f $TMPFILE
+
+# Automatically added by dh_pysupport
+#if which update-python-modules >/dev/null 2>&1; then
+#        update-python-modules  openvpn-applet.private
+#fi
 exit 0
 """
 
     p.changelog="""First Release
 """
 
-#print p.generate(build_binary=False,build_src=True)
 print p.generate(build_binary=True,build_src=True)
